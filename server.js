850a5ac7b46e9c7717d468c5be6cdb4ca4aa434a
loisfa <lois94@hotmail.fr>
2017-10-06 15:30:54 +0200

    added a process.env.PORT as a possible port for express - for heroku deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var path       = require('path');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = /*process.env.PORT ||*/ 8080;        // set our port
+var port = process.env.PORT || 8080;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
